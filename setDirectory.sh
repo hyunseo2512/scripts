3445fb8641cdbd10e156e0dda4f5b051743e9f87
hyunseo2512 <nhs8421@gmail.com>
2026-04-10 16:58:37 +0900

    refactor: setDirectory.sh

--- setDirectory.sh
+++ setDirectory.sh
@@ -2,7 +2,7 @@
 
 cd ~/Desktop
 mkdir -p Backup Workspace
-mkdir -p Backup/log Backup/docs Backup/utils
+mkdir -p Backup/logs Backup/doc
 cd Workspace
-mkdir -p docs record trace scripts vpn
-
+mkdir -p doc record trace scripts vpn tools
+cd
